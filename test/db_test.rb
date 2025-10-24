21202e98bb41924943abdfd6f69c391dea256327
Nick Vladimiroff <nickvladimiroff@gmail.com>
2025-10-23 20:23:30 -0400

    Test: more of them.

--- test/db_test.rb
+++ test/db_test.rb
@@ -15,9 +15,30 @@ class DBTest < ActiveSupport::TestCase
 
   def test_set_get
     @db.set(1, 'hello world')
-    value = @db.get(1)
 
+    value = @db.get(1)
     assert_equal('hello world', value)
+  end
+
+
+  def test_double_set
+    @db.set(1, 'hello world')
+    @db.set(1, 'goodbye world')
+
+    value = @db.get(1)
+    assert_equal('goodbye world', value)
+  end
+
+
+  def test_set_multiple
+    @db.set(1, '1')
+    @db.set(2, '2')
+
+    value = @db.get(1)
+    assert_equal('1', value)
+
+    value = @db.get(2)
+    assert_equal('2', value)
   end
 
 end
